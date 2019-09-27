1b94b8dd87042227496fad1b2795f52ace34e3c3
sharon story smith <sstorysmith@gmail.com>
2019-09-27 10:54:03 -0500

    working app for displaying Gifs - split html into separate files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,121 +1,116 @@
 
 
-  // Initial array of topics
-  const topics = ["sunrise", "sunset", "tropical storms", "storm chasers", "flooding"];
+  //fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff 
+  // Function Declarations:
 
-  // Calling the renderButtons function to display the intial buttons
-    renderButtons();
-
-   // This function handles events where one button is clicked
-    $("#add-topic").on("click", function(event) {
-    event.preventDefault();
-    
-  // This line grabs the input from the textbox
-    let topic = $("#topic-input").val().trim();
-
-  // The topic from the textbox is then added to our array
-    topics.push(topic);
-
-  // Calling renderButtons which handles the processing of our topics array
-    renderButtons();
-
-    });
-
-  //  Function Declarations -------------------------------------------------------------//
-
-
-  // Function for displaying topic buttons
+  // Function for displaying topic buttons -----------------
   function renderButtons() {
-  //Deleting the buttons prior to adding new movies
-  // (this is necessary otherwise you will have repeat buttons)
-    $("#buttons-view").empty();
+      //Deleting the buttons prior to adding new movies
+      // (this is necessary otherwise you will have repeat buttons)
+      $("#buttons-view").empty();
 
-  // Looping through the array of movies
-    for (let i = 0; i < topics.length; i++) {
+    // Looping through the array of movies
+      for (let i = 0; i < topics.length; i++) {
 
-  // Then dynamicaly generating buttons for each movie in the array
-  // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-        let a = $("<button>");
-  // Adding a class of topic to our button
-        a.addClass("topic");
-  // Adding a data-attribute
-        a.attr("data-name", topics[i]);
-  // Providing the initial button text
-        a.text(topics[i]);
-  // Adding the button to the buttons-view div
-        $("#buttons-view").append(a);
-    }
+        // Then dynamicaly generating buttons for each movie in the array
+        // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+              let a = $("<button>");
+        // Adding a class of topic to our button
+              a.addClass("topic");
+        // Adding a data-attribute
+              a.attr("data-name", topics[i]);
+        // Providing the initial button text
+              a.text(topics[i]);
+        // Adding the button to the buttons-view div
+              $("#buttons-view").append(a);
+      }
 
-  // get giffy images and display
-    
-    $(".topic").on("click", function() {
-        const  max=10;
-        let searchTopic= $(this).attr("data-name");
+      // get giffy images and display    
+      $(".topic").on("click", function() {  // ---------------------------
+      const  max=10;
+      let searchTopic= $(this).attr("data-name");
         
-  //  empty the previous gifs
-        $("#giphy-view").empty();
+      //  empty the previous gifs
+      $("#giphy-view").empty();
 
-  // construct giffy api
-         let queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-             searchTopic + "&api_key=vGNioWrNlBT1qhho361JM3kxmUvX8x4F&limit=10&=rating=G";
+      // construct giffy api
+          let queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+          searchTopic + "&api_key=vGNioWrNlBT1qhho361JM3kxmUvX8x4F&limit=10&=rating=G";
          
-  // Performing an AJAX request with the queryURL for .gif
+      // Performing an AJAX request with the queryURL for .gif
         $.ajax({
             url: queryURL,
             method: "GET"
           
         })
 
-  // After data comes back from the request
-          .then(function(response) {
+      // After data comes back from the request
+      .then(function(response) { // --------------------------------
 
-  // storing the data from the AJAX request in the results variable
+            // storing the data from the AJAX request in the results variable
             let results = response.data;
-  // Looping through each result item
+            // Looping through each result item
             for (let i = 0; i < results.length; i++) {
-  // Creating and storing a div tag
+                // Creating and storing a div tag
                 let gifDiv = $("<div>");
-  // Creating a paragraph tag with the result item's rating in upperCase
-                // upCaseRating= changeto.upperCase(results[i].rating);
+                // Creating a paragraph tag with the result item's rating in upperCase
+                let upCaseRating= (results[i].rating).toUpperCase();
+                let p = $("<p>").text("Rating: " + upCaseRating);
+                //let p = $("<p>").text("Rating: " + results[i].rating);
 
-  // let p = $("<p>").text("Rating: " + upCaseRating);
-                let p = $("<p>").text("Rating: " + results[i].rating);
            
-  // Creating and storing an image tag
+                // Creating and storing an image tag
                 let gifImage = $("<img class='gifClass'> ");
-  // Setting the src attribute of the image to a property pulled off the result item
+                // Setting the src attribute of the image to a property pulled off the result item
                 gifImage.attr("src", results[i].images.fixed_height_still.url);
                 gifImage.attr("data-still", results[i].images.fixed_height_still.url);
-  // Appending the paragraph and image tag to the gifDiv
+                // Appending the paragraph and image tag to the gifDiv
                 gifImage.attr("data-state","still");
-                gifImage.attr("data-animate", results[i].images.fixed_height.url);
-               
-
-    
+                gifImage.attr("data-animate", results[i].images.fixed_height.url);    
                 gifDiv.append(p);
                 gifDiv.append(gifImage);
-  // Prependng the gifDiv to the HTML page in the "#gifs-appear-here" div
+                // Prependng the gifDiv to the HTML page in the "#gifs-appear-here" div
                 $("#giphy-view").prepend(gifDiv);
             }
-  // animate/still the .gif that was clicked on
-            $(".gifClass").on("click", function() {
-       
+          // animate/still the .gif that was clicked on
+          $(".gifClass").on("click", function() {   //--------------------     
 
-  // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
-             let state = $(this).attr("data-state");
-  // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-  // Then, set the image's data-state to animate
-  // Else set src to the data-still value
+            // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+            let state = $(this).attr("data-state");
+            // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+            // Then, set the image's data-state to animate
+            // Else set src to the data-still value
             if (state === "still") {
                 $(this).attr("src", $(this).attr("data-animate"));
                 $(this).attr("data-state", "animate");
             } else {
                 $(this).attr("src", $(this).attr("data-still"));
                 $(this).attr("data-state", "still");
-              }
-            });
+            }
+          });
 
         });
     });
   }
+  //ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
 
+    // Initial array of topics
+    const topics = ["sunrise", "sunset", "tropical storms", "storm chasers", "flooding"];
+
+    // Calling the renderButtons function to display the intial buttons
+      renderButtons();
+  
+     // This function handles events where one button is clicked
+      $("#add-topic").on("click", function(event) {
+      event.preventDefault();
+      
+        // This line grabs the input from the textbox
+          let topic = $("#topic-input").val().trim();
+  
+        // The topic from the textbox is then added to our array
+          topics.push(topic);
+  
+        // Calling renderButtons which handles the processing of our topics array
+          renderButtons();
+  
+      });
--- readme.md
+++ readme.md
@@ -1,30 +1,30 @@
-2019_08_30
-What the project does:
-    This Homework #6 loads up an array of movie topics Sharon is interested in:
+<!-- Sharon Story Smith  sstorysmith@gmail.com
+2019-08-30 
+What ths application does:
+    This appliction loads up an array of .gif topics Sharon is interested in:
                    Sunrise, sunset, tropical storms, storm chasers, flooding, 
     Each topic generates a button with the topic name. 
 
-    A form is used to take the value from the user input box. The input is added
+    A form is used to take a topic name from the user input box. The input is added
         into the`topics` array and a function call takes each topic in the array and adds
-        a button on the page.
-    When the user clicks on a button, the page pulls in 10 static, non-animatede gif images 
-        from the GIPHY API and place them on the page along with a  rating (PG, G, ...).
+        a button with that topic name onto the page.
+    When the user clicks on a topic button, the page pulls in 10 static, non-animated .gif images 
+        from the GIPHY API and places them on the page along with a  rating (PG, G, ...).
     When the user clicks one of the still GIPHY images, the gif animates. If the user
-         clicks the gif again, it stops playing.
+         clicks the gif again, it stops playing, it toggles.
 
 Why the project is useful:
-    This project is useful taking user input, finding the resources, then adding clickable
+    This project is useful taking user input from the browser screen, finding the .gif resources, then adding clickable
     links to facilitate ease of access --- a very common requirement from users.
 
 How users can get started with the project
+    The screen will display a group of buttons with movie topic interests. Cick on a chosen button to 
+    a max of 10 .gifs of that topic. 
+    If you would like to add new topic buttons with user entered topics of interest, type in your
+    "interest", and click submit. If you would like to watch any of the .gifs displayed, click on
+    the the image of your choice. If the .gif is playing, click to pause, and toggle back and forth.
 
-    The screen will display a group of buttons with movie topic interests. Click on a chosen button to
-    load a max of 10 giphys based on that interests.  If you would like to add new buttons with
-    user entered topics of interest, ty[e in your "interest", and click submit. If you would like
-    to watch any of the giphs displayed, click on the the image of your choice. If the giphy is
-    playing, click to pause.
-
-    Start this project by reviewing the code from classroom coding examples:
+    this project was completed with these references from classroom coding examples:
             class 8/22/2019 - Add movie buttons: movies.solved 8/22
             class 8/24/2019- Start stop giphys: pausing Gifs.solved 
             class 8/22/2019 - Adding elements, displaying gifs from a button click: Button triggered ajax
@@ -33,6 +33,4 @@ Where users can get help with your project:
     If you need help with this code, contact Sstorysmith@gmail.com.
 
 Who maintains and contributes to the project:
-    Sharon Story Smith maintains and contributes to this code.
-
-
+    Sharon Story Smith maintains and contributes to this code, sstorysmith@gmail.com -->
